73bb438d335750e4cffc30be8803bea6191cb9d3
COCF392 <chrisofeliz@gmail.com>
2024-08-13 21:46:55 -0400

    mvp actually complete now

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// references
+// references and other vars
 const schoolSelector = document.querySelector("select");
 const subHeaderText = document.getElementById("sub_header");
 const sideBarButton = document.getElementById("side_carrot");
 const sideNav = document.getElementById("side_nav");
 const welcomeBox = document.getElementById("welcome_box");
 const forumHeader = document.getElementById("current_forum_header");
+const titleInput = document.getElementById("title_input_box");
+const bodyInput = document.getElementById("body_input_box");
+const titleResult = document.getElementById("title_text_box");
+const bodyResult = document.getElementById("body_text_box");
+const submitButton = document.getElementById("submit_button");
+const answerHeader = document.getElementById("answer_header");
 let schoolArray = [];
 
+// fetching the school API for names
 fetch("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/us-public-schools/records?limit=100&refine=state%3A%22NY%22")
     .then(((response) => {
         return response.json();
@@ -29,16 +36,43 @@ fetch("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/us-publ
         }
     })
 
-schoolSelector.addEventListener("input", () => {
-    subHeaderText.innerText = `You are currently in the ${schoolSelector.value} forum.`
-})
+// listening for selections in the homepage
+if (subHeaderText) {
+    schoolSelector.addEventListener("input", () => {
+        subHeaderText.innerText = `You are currently in the ${schoolSelector.value} forum.`;
+        localStorage.setItem("storedSchool", schoolSelector.value);
+        
+    });
+}
 
-// moving the side_bar left and right
+// moving the side bar left and right
 sideBarButton.addEventListener("click", () => {
     sideNav.classList.toggle("move_left");
     sideBarButton.classList.toggle("flip_carrot");
-})
+});
 
-schoolSelector.addEventListener("input", () => {
-    forumHeader.innerText = `Posting in ${schoolSelector.value}`
-})
+// submitting text to answer page
+try {
+    submitButton.addEventListener("click", () => {
+        localStorage.setItem("storedTitle", titleInput.value);
+        localStorage.setItem("storedBody", bodyInput.value);
+    })
+}
+catch {
+    console.log("Button not in page")
+}
+
+// for the answer page after text has been input
+if (bodyResult && titleResult) {
+    titleResult.innerText = localStorage.getItem("storedTitle");
+    bodyResult.innerText = localStorage.getItem("storedBody");
+}
+
+// for the question page
+if (forumHeader) {
+    forumHeader.innerText = `Posting in ${localStorage.getItem("storedSchool")}`;
+}
+
+if (answerHeader) {
+    answerHeader.innerText = `${localStorage.getItem("storedSchool")} Forum`
+}
